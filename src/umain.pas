2b0c58c7b907a9c6de63a7d041b0126cfc059413
Vinícius Pontin <vpontin93@gmail.com>
2017-07-28 23:40:18 -0300

    Adicionada toolbar
    Com imagens toscas do font-awesome

--- src/umain.pas
+++ src/umain.pas
@@ -5,14 +5,15 @@ unit uMain;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, Menus,
-  ComCtrls;
+  Classes, SysUtils, FileUtil, SpkToolbar, spkt_Tab, spkt_Pane, spkt_Buttons,
+  spkt_Checkboxes, Forms, Controls, Graphics, Dialogs, Menus, ComCtrls;
 
 type
 
   { TfPrincipal }
 
   TfPrincipal = class(TForm)
+    il4tbPrincipal: TImageList;
     miAjuda: TMenuItem;
     msConteudo: TMenuItem;
     msTutorial: TMenuItem;
@@ -27,8 +28,24 @@ type
     msSeparador1: TMenuItem;
     mmPrincipal: TMainMenu;
     sbPrincipal: TStatusBar;
+    SpkLargeButton1: TSpkLargeButton;
+    SpkPane1: TSpkPane;
+    SpkRadioButton1: TSpkRadioButton;
+    SpkTab1: TSpkTab;
+    SpkTab2: TSpkTab;
+    SpkTab3: TSpkTab;
     tbPrincipal: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+    ToolButton5: TToolButton;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ToolButton9: TToolButton;
     procedure msSairClick(Sender: TObject);
+    procedure msSobreClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -47,6 +64,11 @@ implementation
 procedure TfPrincipal.msSairClick(Sender: TObject);
 begin
   Application.Terminate;
+end;
+
+procedure TfPrincipal.msSobreClick(Sender: TObject);
+begin
+
 end;
 
 
